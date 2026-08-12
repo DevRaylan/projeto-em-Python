f697a05e14f6e230ada1dd64d8157575b56322f6
Raylan Araujo <116830259+DevRaylan@users.noreply.github.com>
2026-08-12 10:06:24 -0300

    Delete SA/SA Desempenho Acadêmico 3.6v directory
    Signed-off-by: Raylan Araujo <116830259+DevRaylan@users.noreply.github.com>

--- SA/SA Desempenho Acadêmico 3.6v/professor.py
+++
@@ -1,6 +0,0 @@
-class Professor:
-    def __init__(self, nome, disciplina, codigo_disciplina, ano):
-        self.nome = nome
-        self.disciplina = disciplina
-        self.codigo_disciplina = codigo_disciplina
-        self.ano = ano
