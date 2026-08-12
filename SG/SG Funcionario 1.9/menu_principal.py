4b95483bea1a643d8ce013c280f0fd94ddfa39a4
Raylan Araujo <116830259+DevRaylan@users.noreply.github.com>
2026-08-12 10:08:27 -0300

    Delete SG/SG Funcionario 1.9 directory
    Signed-off-by: Raylan Araujo <116830259+DevRaylan@users.noreply.github.com>

--- SG/SG Funcionario 1.9/menu_principal.py
+++
@@ -1,6 +0,0 @@
-def menu_principal():
-    print("\n--- Menu Principal ---")
-    print("1. Funcionario")
-    print("2. Criar Backup de Dados")
-    print("3. Recuperar Backup de Dados")
-    print("4. Sair")
